009f999aa4fb1619a40484fdb1dee26935679903
Rocamain <fjrocavazquez@gmail.com>
2023-09-11 19:48:55 +0100

    Finalized EventInput component

--- src/components/Event/EventInput/EventInput.tsx
+++ src/components/Event/EventInput/EventInput.tsx
@@ -1,12 +1,5 @@
 'use client'
-import {
-  useState,
-  ChangeEvent,
-  useRef,
-  useEffect,
-  MouseEvent,
-  FocusEvent,
-} from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 interface InputProps {
   data: string
   label?: string
@@ -26,91 +19,102 @@ interface InputProps {
   className: string
 }
 
-export const EventInput = ({ data, type, name, className }: InputProps) => {
+export const EventInput = ({
+  data,
+  type,
+  name,
+  className,
+  pattern,
+}: InputProps) => {
   const [inputData, setInputData] = useState<string>(data)
-  const [dataToSend, setDataToSend] = useState<string | undefined>()
   const [isEdit, setEdit] = useState<boolean>(false)
-  const [saved, setSave] = useState<boolean>(false)
   const inputRef = useRef<HTMLInputElement | null>(null)
-  const [regexError, setRegexError] = useState<boolean>(false)
   const [isTyped, setTyped] = useState<boolean>(false)
+  const [isRegexValid, setRegexValid] = useState<boolean>(true)
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
+  // Manages the focus or blur depending on whether the "Edit" button is pressed
+  useEffect(() => {
+    if (isEdit && inputRef.current) {
+      inputRef.current.focus()
+    } else if (inputRef.current) {
+      inputRef.current.blur()
+    }
+  }, [isEdit])
+
+  const isInitialValueChange = data !== inputData
+
+  const inputStyleRead: React.CSSProperties = {
+    width: `${inputData.length * 14}px`,
+  }
+  const inputStyleEdit: React.CSSProperties = {
+    width: `${(inputData.length + 2) * 14}px`,
+  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, pattern } = e.target
     setInputData(value)
     setTyped(true)
-    if (!value.match(pattern)) {
-      setRegexError(true)
-    } else {
-      setRegexError(false)
-    }
+    setRegexValid(value.match(pattern) !== null)
   }
 
-  useEffect(() => {
-    if (inputRef.current) {
-      if (isEdit) {
-        inputRef.current.focus()
-      } else {
-        inputRef.current.blur()
-      }
-    }
-  }, [isEdit, inputRef])
-
-  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-    // e.stopPropagation()
-    if (e.currentTarget.innerText === '...Edit') {
-      setDataToSend(inputData)
-    }
-
+  const handleClick = () => {
     setEdit((prev) => !prev)
-  }
-  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
-    if (e.relatedTarget?.innerHTML !== 'Save') {
-      setInputData(dataToSend as string)
-      setEdit(false)
-    } else {
-      setSave(true)
-      setEdit(true)
-    }
-    setTyped(false)
   }
 
   return (
     <>
-      {isEdit || saved ? (
-        <div className="relative flex">
+      <div className="relative flex">
+        {isEdit ? (
+          // Render an input field when in edit mode
           <input
             ref={inputRef}
-            id={name}
+            id="eventTitle"
             name={name}
-            className={className}
+            className={
+              'p-2 text-2xl xl:text-3xl font-bold rounded-lg w-inherit overflow-hidden focus:outline-none focus:ring focus:border-blue-400 focus:rounded-lg focus:w-auto'
+            }
+            style={inputStyleEdit}
             type={type}
             value={inputData}
+            pattern={pattern}
             onChange={handleChange}
-            onBlur={handleBlur}
-            pattern="^(.{8,20})$"
-            disabled={!dataToSend}
+            size={inputData.length}
           />
+        ) : // If the initial value has changed, render a read-only input
+        // The form action will receive the info to be sent
+        !isInitialValueChange ? (
+          <h3 className={className}>{inputData}</h3>
+        ) : (
+          <input
+            id={name}
+            name={name}
+            className={
+              'pl-2 text-2xl xl:text-3xl font-bold rounded-lg p-2 w-inherit overflow-hidden focus:outline-none focus:ring focus:border-blue-400 focus:rounded-lg focus:w-auto'
+            }
+            style={inputStyleRead}
+            type={type}
+            value={inputData}
+            readOnly
+          />
+        )}
+        {/* If in edit mode and isTyped is true, render an 'x' or a '✓' depending on regex validation */}
+        {isTyped && isEdit && (
           <div className="pl-2 w-5 absolute right-2">
-            {isTyped && regexError && (
+            {isRegexValid ? (
+              <span className="text-xl text-green-500">✓</span>
+            ) : (
               <span className="text-xl text-red-500">✖</span>
             )}
-            {isTyped && !regexError && (
-              <span className="text-xl text-green-500">✓</span>
-            )}
           </div>
-        </div>
-      ) : (
-        <h3 className={className}>{inputData}</h3>
-      )}
+        )}
+      </div>
 
+      {/* Render the "Edit" or "Save" button */}
       <button
         onClick={handleClick}
         type="button"
-        className="text-gray-400 hover:text-red-400  text-xl font-bold"
+        className="text-gray-400 hover:text-red-400 text-xl font-bold"
       >
-        {isEdit ? 'Save' : '...Edit'}
+        {isEdit ? 'Save' : 'Edit'}
       </button>
     </>
   )
