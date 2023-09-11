dfd93e17ccac0a602e8d51704251bcba9a33f445
Rocamain <fjrocavazquez@gmail.com>
2023-09-11 19:54:16 +0100

    Finalized EventTitle component

--- src/components/Event/EventHeader/EventTitle.tsx
+++ src/components/Event/EventHeader/EventTitle.tsx
@@ -10,24 +10,27 @@ export const EventTitle: React.FC<EventTitleProps> = ({
   eventTitle,
   isOwner,
 }) => {
-  const className = 'pl-2 text-2xl xl:text-3xl font-bold'
+  const className = 'p-2 text-2xl xl:text-3xl font-bold'
   return (
     <div className="flex gap-5">
-      {isOwner ? (
-        <EventInput
-          className={className + ' focus:bg-slate-100'}
-          name="eventTitle"
-          type="text"
-          data={eventTitle}
-        />
-      ) : (
-        <h3 className={className}>{eventTitle}</h3>
-      )}
-      {isOwner && (
-        <button name="submit" value="Send Request" type="submit">
-          Modify
-        </button>
-      )}
+      {
+        // If the user is the owner of the event, render an EventInput component that allows editing the EventTitle
+        isOwner ? (
+          <EventInput
+            className={
+              className +
+              ' rounded-lg p-2 w-inherit overflow-hidden focus:outline-none focus:ring focus:border-blue-400 focus:rounded-lg'
+            }
+            name="eventTitle"
+            type="text"
+            pattern="^(.{8,40})$"
+            data={eventTitle}
+          />
+        ) : (
+          // If the user is not the owner, display the EventTitle as plain text
+          <h3 className={className}>{eventTitle}</h3>
+        )
+      }
     </div>
   )
 }
