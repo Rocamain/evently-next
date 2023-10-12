72e7edd711f8e8dd19b4d845bc63d6bf0c25c8af
Rocamain <fjrocavazquez@gmail.com>
2023-10-12 12:50:58 +0100

    Improve UI and feat on EventId route

--- src/components/Event/EventHeader/EventHeader.tsx
+++ src/components/Event/EventHeader/EventHeader.tsx
@@ -5,14 +5,20 @@ import React from 'react'
 interface EventHeaderProps {
   children: React.ReactNode
   eventOwnerName: string
+  eventOwnerPicture: string
 }
 
-export const EventHeader = ({ children, eventOwnerName }: EventHeaderProps) => {
+export const EventHeader = ({
+  children,
+  eventOwnerName,
+  eventOwnerPicture,
+}: EventHeaderProps) => {
+  console.log(eventOwnerPicture)
   return (
     <div className="mx-4 pt-6 pb-6">
       {children}
-      <Link href="#" className="flex flex-row mt-2 lg:mt-4">
-        <UserPicture name={eventOwnerName} />
+      <Link href="/#" className="flex flex-row items-center mt-2 lg:mt-4">
+        <UserPicture picture={eventOwnerPicture} name={eventOwnerName} />
         <div className="ml-3">
           <p className="xl:text-lg">Hosted by</p>
           <p className="xl:text-xl font-bold">{eventOwnerName}</p>
--- src/components/Event/EventInfo/EventInfo.tsx
+++ src/components/Event/EventInfo/EventInfo.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { TimeIcon, LocationIcon, WebsiteLinkIcon } from '@/components/Icons'
-import { LinkButton } from '@/components/Shared/LinkButton/LinkButton'
 import Link from 'next/link'
 
 interface EventInfoProps {
@@ -15,11 +14,8 @@ export const EventInfo: React.FC<EventInfoProps> = ({
   eventLocation,
 }) => {
   return (
-    <div
-      className="
-    "
-    >
-      <div className="lg:ml-8 xl:ml-24 lg:min-w-[280px] lg:max-w-[250px] border-b border-shadowColor">
+    <div>
+      <div className="lg:ml-8 xl:ml-24 lg:min-w-[280px] lg:max-w-[250px] border-b border-shadowColor sticky top-3">
         <div className="pb-6 border-b border-shadowColor lg:bg-white sm:rounded-t-2xl lg:pt-7">
           <div className="flex flex-col mx-4">
             <div className="flex">
